7a6b3ce196a8a1110737468824bbf2324620de9d
Celica Sylphil <kamikitakomaric@protonmail.com>
2025-07-04 20:26:07 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -3,4 +3,5 @@
 
 if exist ".\build\" rmdir /s /q ".\build"
 cmake -S . -B build -G "Visual Studio 17 2022" -A win32 && cmake --build build --config Release
+copy .\FilesDumper.ini .\build\Release
 pause
